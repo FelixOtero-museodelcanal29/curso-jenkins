866d789e364e58d6c2443734a51f45fe62ee6b2f
Felix Otero <fotero@museodelcanal.com>
2025-12-29 16:13:22 -0500

    fix:v13 correccion en run_tests.sh

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -17,6 +17,7 @@ pip install -r requirements.txt --break-system-packages
 echo "Ejecutando pruebas con pytest"
 echo "version de python"
 venv/bin/python --version
+venv/bin/python -m pytest --version
 venv/bin/python -m pytest tests/  --junitxml=reports/test_results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas Finalizadas  resultados en reports"
