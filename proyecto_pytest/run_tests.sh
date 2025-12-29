7f164d7b6767eb060f3d4b911dfaf041643fdb42
Felix Otero <fotero@museodelcanal.com>
2025-12-29 09:35:14 -0500

    correccion de entorno virtual

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 echo "Activandpo el entorno virtual"
+if [ ! -d "venv" ]; then
+    echo "Creando entorno virtual"
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
 
 echo "instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Ejecutando pruebas con pytest"
