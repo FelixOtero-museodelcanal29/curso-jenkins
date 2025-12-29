5671ddc0382a5d6e7e3826bf0b06e3b566e1b85e
Felix Otero <fotero@museodelcanal.com>
2025-12-29 09:49:04 -0500

    correcion de sripts run_tests.sh

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-echo "Activandpo el entorno virtual"
+echo "Iniciando script de pruebas"
 if [ ! -d "venv" ]; then
     echo "Creando entorno virtual"
     python3 -m venv venv
 fi
-
-source venv/bin/activate
+# Activar el entorno virtual
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then
+    source venv/Scripts/activate
+else
+    echo "No se pudo encontrar el script de activación del entorno virtual."
+    exit 1
+fi
 
 echo "instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "Ejecutando pruebas con pytest"
 pytest tests/  --junitxml=reports/test_results.xml --html=reports/test-results.html --self-contained-html
